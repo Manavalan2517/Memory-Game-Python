fbba94a18ac8382bb05185ae12e0f647b1d78681
Mano <112639423+Manavalan2517@users.noreply.github.com>
2024-05-13 22:18:24 +0530

    Update main.py

--- Memory Game Python/main.py
+++ Memory Game Python/main.py
@@ -1,27 +1,28 @@
 import random
 from tabulate import tabulate
+import os
 
-values = {
-    'a' : 0,
-    'b' : 1,
-    'c' : 2,
-    'd' : 3,
-    'e' : 4,
-    'f' : 5,
-    'g' : 6,
-    'h' : 7
-}
-header = [":)", "0", "1", "2", "3", "4", "5", "6", "7"]
+values = {'a' : 0, 'b' : 1, 'c' : 2, 'd' : 3, 'e' : 4, 'f' : 5, 'g' : 6, 'h' : 7}
 temp_found = []
 user_values = []
+user_choices = []
 progress = True
-won = False
+competed = False
 
-difficulty_choice = int(input("""
-Choose the Difficulty:
-Press (1) for Easy Mode
-Press (2) for Medium Mode
-Press (3) for Hard Mode
+user_choice_data = int(input("""
+Eg1:- for '2x2' enter 2
+Eg2:- for '4x4' enter 4       
+NOTE:- Enter only EVEN numbers
+
+Enter your choice: """))
+
+while (user_choice_data%2) != 0:
+    os.system("cls")
+    user_choice_data = int(input("""
+Eg1:- for '2x2' enter 2
+Eg2:- for '4x4' enter 4            
+NOTE:- Enter only EVEN numbers
+
 Enter your choice: """))
 
 def game_elements(num):
@@ -36,15 +37,27 @@ def game_elements(num):
         element = row.copy()
         random.shuffle(element)
         lst.append(element)
-        random.shuffle(lst)
     return lst
 
-if difficulty_choice == 1:
-    main = game_elements(4)
+def game_process(num):
+    global user_values
+    global temp_found
+    global competed
+
     list_with_spaces = [[' ' for _ in sublist] for sublist in main]
     while progress:
+        os.system('cls')
+        print(tabulate(list_with_spaces, tablefmt="rounded_grid"))
+        trial = 0
         for i in range(2):
+            if len(user_choices) == (num**2):
+                competed = True
+                break
+
             opn = input("\nEnter the tile to uncover: ")
+            while opn in user_choices:
+                print("\n===========>>>You have already completed this tile!")
+                opn = input("\nEnter the new tile to uncover: ")
             user_values.append(opn)
 
             index_1 = values[opn[0]]
@@ -53,11 +66,18 @@ if difficulty_choice == 1:
             temp_found.append(main[index_1][index_2])
             
             list_with_spaces[index_1][index_2] = main[index_1][index_2]
-            #print(tabulate(list_with_spaces, header , showindex="always", tablefmt="rounded_grid"))
             print(tabulate(list_with_spaces, tablefmt="rounded_grid"))
+            trial += 1
+            if trial == 2:
+                user_input = input("\n===========>>> Press (Enter) to Continue: ")
+                trial = 0
 
-        if temp_found[0] == temp_found[1]:
+        if competed:
+            print("\n===========>>> You Won! <<<===========")
+            break
+        elif temp_found[0] == temp_found[1]:
             for i in range(2):
+                user_choices.append(user_values[i])
                 index_1 = values[user_values[i][0]]
                 index_2 = int(user_values[i][1])
                 list_with_spaces[index_1][index_2] = "✅"
@@ -70,11 +90,14 @@ if difficulty_choice == 1:
                 list_with_spaces[index_1][index_2] = " "
             user_values = []
             temp_found = []
+while True:
+    main = game_elements(user_choice_data)
+    process = game_process(user_choice_data)
 
-if difficulty_choice == 2:
-    main = game_elements(6)
-    print(main)
-
-if difficulty_choice == 3:
-    main = game_elements(8)
-    print(main)
+    choice = input("Do you want to play the game again? (y/n): ")
+    while choice in ["y", "n"]:
+        print("\n Invalid choice")
+        choice = input("Do you want to play the game again? (y/n): ")
+    
+    if choice == "n":
+        break
